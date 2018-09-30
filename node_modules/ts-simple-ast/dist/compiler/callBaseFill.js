9b9c4999bff1499aaf85fd6d674e1e261e4de7ef
Kitson Kelly <me@kitsonkelly.com>
2018-09-30 18:40:02 +1000

    Upgrade to ts-simple-ast v16

--- node_modules/ts-simple-ast/dist/compiler/callBaseFill.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// todo: add code verification to ensure all fill functions call this
-/** @internal */
-function callBaseFill(basePrototype, node, structure) {
-    if (basePrototype.fill != null)
-        basePrototype.fill.call(node, structure);
-}
-exports.callBaseFill = callBaseFill;
--- node_modules/ts-simple-ast/dist/structurePrinters/file/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./ExportAssignmentStructurePrinter"), exports);
-tslib_1.__exportStar(require("./ExportDeclarationStructurePrinter"), exports);
-tslib_1.__exportStar(require("./ImportDeclarationStructurePrinter"), exports);
-tslib_1.__exportStar(require("./NamedImportExportSpecifierStructurePrinter"), exports);
-tslib_1.__exportStar(require("./SourceFileStructurePrinter"), exports);
