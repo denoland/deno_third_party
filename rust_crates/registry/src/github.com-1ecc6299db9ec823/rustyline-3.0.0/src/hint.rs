2e41cca17e585bba70bd4e945290db3d4e4ae5d1
Bert Belder <bertbelder@gmail.com>
2019-01-14 03:35:20 +0100

    Upgrade rust crates that require `--edition 2018`

---
+++ rust_crates/registry/src/github.com-1ecc6299db9ec823/rustyline-3.0.0/src/hint.rs
@@ -0,0 +1,16 @@
+//! Hints (suggestions at the right of the prompt as you type).
+
+/// Hints provider
+pub trait Hinter {
+    /// Takes the currently edited `line` with the cursor `pos`ition and
+    /// returns the string that should be displayed or `None`
+    /// if no hint is available for the text the user currently typed.
+    // TODO Validate: called while editing line but not while moving cursor.
+    fn hint(&self, line: &str, pos: usize) -> Option<String>;
+}
+
+impl Hinter for () {
+    fn hint(&self, _line: &str, _pos: usize) -> Option<String> {
+        None
+    }
+}
