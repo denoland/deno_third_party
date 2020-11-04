76b283c420be9302309e94394d1b3f1ebb425f29
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-11-04 22:45:54 +0100

    remove python_packages/

--- python_packages/pylint/test/functional/generated_members.py
+++
@@ -1,8 +0,0 @@
-"""Test the generated-members config option."""
-from __future__ import print_function
-
-class Klass(object):
-    """A class with a generated member."""
-
-print(Klass().DoesNotExist)
-print(Klass().aBC_set1)
--- python_packages/pylint/test/functional/reimported.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=missing-docstring,unused-import,import-error
-
-from time import sleep, sleep # [reimported]
-from lala import missing, missing # [reimported]
-
-import missing1
-import missing1 # [reimported]
